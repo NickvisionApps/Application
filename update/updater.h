b9031cf9d93bcf0fd3885d5a6ec0c90a5df9d833
Nick Logozzo <nlogozzo225@gmail.com>
2022-05-28 21:24:35 -0400

    Updater Fixes
    Removed validateUpdate as it was causing more problems then it was solving.

--- update/updater.h
+++ update/updater.h
@@ -26,6 +26,5 @@ namespace NickvisionApplication::Update
         std::optional<UpdateConfig> m_updateConfig;
         bool m_updateAvailable;
         bool m_updateSuccessful;
-        bool validateUpdate(const std::string& pathToUpdate);
     };
 }
