cdeae0077e84f1ffd9dd0fe13ce817fb42da8bd8
Nick Logozzo <nlogozzo225@gmail.com>
2022-08-13 13:50:46 -0400

    Update MainWindow.cpp

--- NickvisionApplication/UI/Views/MainWindow.cpp
+++ NickvisionApplication/UI/Views/MainWindow.cpp
@@ -73,7 +73,7 @@ namespace NickvisionApplication::UI::Views
 			int result{ msgUpdate.exec() };
 			if (result == QMessageBox::StandardButton::Yes)
 			{
-				ProgressDialog updateDialog{ this, "Downloading and installing the update...", [&]() { m_updater.windowsUpdate(this); } };
+				ProgressDialog updateDialog{ this, "Downloading and installing the update...", [&]() { m_updater.windowsUpdate(); } };
 				updateDialog.exec();
 				if (!m_updater.getUpdateSuccessful())
 				{
