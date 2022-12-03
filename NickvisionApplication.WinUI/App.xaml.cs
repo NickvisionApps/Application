f772f425b77816f9fd8c446c563626e0943a2474
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-03 01:25:22 -0500

    Start WinUI Project

---
+++ NickvisionApplication.WinUI/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.UI.Xaml;
+using NickvisionApplication.Shared;
+using NickvisionApplication.WinUI.Views;
+
+namespace NickvisionApplication.WinUI;
+
+public partial class App : Application
+{
+    private Window _mainWindow;
+
+    public App()
+    {
+        InitializeComponent();
+        //Set AppInfo
+    }
+
+    protected override void OnLaunched(LaunchActivatedEventArgs args)
+    {
+        _mainWindow = new MainWindow();
+        _mainWindow.Activate();
+    }
+}
---
+++ NickvisionApplication.WinUI/UI/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace NickvisionApplication.WinUI.Views;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
