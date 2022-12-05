58544fede914820ae26de48b4ebebe0ec199790e
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-05 08:13:05 -0500

    GNOME - Basic Window

--- NickvisionApplication.GNOME/Program.cs
+++ NickvisionApplication.GNOME/Program.cs
@@ -9,6 +9,17 @@ public class Program
         application.OnActivate += (sender, e) =>
         {
             var window = Adw.ApplicationWindow.New(application);
+            window.SetDefaultSize(800, 600);
+            var box = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
+            var headerBar = Adw.HeaderBar.New();
+            var button = Gtk.Button.New();
+            var buttonContent = Adw.ButtonContent.New();
+            buttonContent.SetLabel("Open");
+            buttonContent.SetIconName("folder-symbolic");
+            button.SetChild(buttonContent);
+            headerBar.PackStart(button);
+            box.Append(headerBar);
+            window.SetContent(box);
             window.Show();
         };
         return application.Run();
