a1d93fcfe173f0e31f14d26a3cc7c7264050656d
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-05 13:00:34 -0500

    Add MainWindow Class

--- NickvisionApplication.GNOME/Program.cs
+++ NickvisionApplication.GNOME/Program.cs
@@ -1,27 +1,49 @@
-﻿namespace NickvisionApplication.GNOME;
+﻿using NickvisionApplication.GNOME.Views;
+using NickvisionApplication.Shared.Controllers;
+using NickvisionApplication.Shared.Models;
+using System;
+
+namespace NickvisionApplication.GNOME;
 
 public class Program
 {
-    public static int Main(string[] args)
+    private readonly Adw.Application _application;
+
+    public static int Main(string[] args) => new Program().Run();
+
+    public Program()
     {
         Adw.Module.Initialize();
-        var application = Adw.Application.New("org.nickvision.application", Gio.ApplicationFlags.FlagsNone);
-        application.OnActivate += (sender, e) =>
+        _application = Adw.Application.New("org.nickvision.application", Gio.ApplicationFlags.FlagsNone);
+        _application.OnActivate += OnActivate;
+    }
+
+    public int Run() => _application.Run();
+
+    private void OnActivate(object sedner, EventArgs e)
+    {
+        //Controller Setup
+        var mainWindowController = new MainWindowController();
+        mainWindowController.AppInfo.ID = "org.nickvision.application";
+        mainWindowController.AppInfo.Name = "NickvisionApplication";
+        mainWindowController.AppInfo.ShortName = "Application";
+        mainWindowController.AppInfo.Description = "A template for creating Nickvision applications.";
+        mainWindowController.AppInfo.Version = "2022.12.0-next";
+        mainWindowController.AppInfo.Changelog = "<ul><li>Initial Release</li></ul>";
+        mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/nlogozzo/NickvisionApplication");
+        mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/nlogozzo/NickvisionApplication/issues/new");
+        mainWindowController.AppInfo.SupportUrl = new Uri("https://github.com/nlogozzo/NickvisionApplication/discussions");
+        //Set Adw Theme
+        _application.StyleManager!.ColorScheme = mainWindowController.Theme switch
         {
-            var window = Adw.ApplicationWindow.New(application);
-            window.SetDefaultSize(800, 600);
-            var box = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
-            var headerBar = Adw.HeaderBar.New();
-            var button = Gtk.Button.New();
-            var buttonContent = Adw.ButtonContent.New();
-            buttonContent.SetLabel("Open");
-            buttonContent.SetIconName("folder-symbolic");
-            button.SetChild(buttonContent);
-            headerBar.PackStart(button);
-            box.Append(headerBar);
-            window.SetContent(box);
-            window.Show();
+            Theme.System => Adw.ColorScheme.PreferLight,
+            Theme.Light => Adw.ColorScheme.ForceLight,
+            Theme.Dark => Adw.ColorScheme.ForceDark,
+            _ => Adw.ColorScheme.PreferLight
         };
-        return application.Run();
+        //Main Window
+        var mainWindow = new MainWindow(mainWindowController);
+        _application.AddWindow(mainWindow);
+        mainWindow.Show();
     }
 }
---
+++ NickvisionApplication.GNOME/Views/MainWindow.cs
@@ -0,0 +1,27 @@
+﻿using NickvisionApplication.Shared.Controllers;
+
+namespace NickvisionApplication.GNOME.Views;
+
+public class MainWindow : Adw.ApplicationWindow
+{
+    private readonly MainWindowController _controller;
+    private readonly Gtk.Box _mainBox;
+    private readonly Adw.HeaderBar _headerBar;
+    private readonly Adw.WindowTitle _windowTitle;
+
+    public MainWindow(MainWindowController controller)
+    {
+        //Window Settings
+        _controller = controller;
+        SetDefaultSize(800, 600);
+        //Main Box
+        _mainBox = Gtk.Box.New(Gtk.Orientation.Vertical, 0);
+        //Header Bar
+        _headerBar = Adw.HeaderBar.New();
+        _windowTitle = Adw.WindowTitle.New(_controller.AppInfo.ShortName, "");
+        _headerBar.SetTitleWidget(_windowTitle);
+        _mainBox.Append(_headerBar);
+        //Layout
+        SetContent(_mainBox);
+    }
+}
