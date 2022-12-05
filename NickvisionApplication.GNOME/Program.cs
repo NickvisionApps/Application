7a47b4fd26e2c976b9ae9dacc603bedbeb29f43e
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-04 23:55:08 -0500

    GNOME - Start

---
+++ NickvisionApplication.GNOME/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace NickvisionApplication.GNOME;
+
+public class Program
+{
+    public static int Main(string[] args)
+    {
+        var application = Adw.Application.New("org.nickvision.application", Gio.ApplicationFlags.FlagsNone);
+        application.OnActivate += (sender, e) =>
+        {
+            var window = Adw.ApplicationWindow.New(application);
+            window.Show();
+        };
+        return application.Run();
+    }
+}
