862e8edf78ba2d957c9a3770dac821ce58f11f6e
Nick Logozzo <nlogozzo225@gmail.com>
2022-12-06 09:38:57 -0500

    GNOME - Theme Change

--- NickvisionApplication.GNOME/Views/PreferencesDialog.cs
+++ NickvisionApplication.GNOME/Views/PreferencesDialog.cs
@@ -7,19 +7,21 @@ namespace NickvisionApplication.GNOME.Views;
 public class PreferencesDialog : Adw.Window
 {
     private readonly PreferencesViewController _controller;
+    private readonly Adw.Application _application;
     private readonly Gtk.Box _mainBox;
     private readonly Adw.HeaderBar _headerBar;
     private readonly Adw.PreferencesPage _page;
     private readonly Adw.PreferencesGroup _grpUserInterface;
     private readonly Adw.ComboRow _rowTheme;
 
-    public PreferencesDialog(PreferencesViewController controller, Gtk.Window parent)
+    public PreferencesDialog(PreferencesViewController controller, Adw.Application application, Gtk.Window parent)
     {
         //Window Settings
         _controller = controller;
+        _application = application;
         New();
         SetTransientFor(parent);
-        SetDefaultSize(800, 600);
+        SetDefaultSize(600, 400);
         SetModal(true);
         SetDestroyWithParent(false);
         SetHideOnClose(true);
@@ -38,6 +40,7 @@ public class PreferencesDialog : Adw.Window
         _grpUserInterface.SetDescription("Customize the application's user interface.");
         _rowTheme = Adw.ComboRow.New();
         _rowTheme.SetTitle("Theme");
+        _rowTheme.SetSubtitle("A theme change will be applied when the dialog is closed.");
         _rowTheme.SetModel(Gtk.StringList.New(new string[] { "Light", "Dark", "System" }));
         _grpUserInterface.Add(_rowTheme);
         _page.Add(_grpUserInterface);
@@ -51,6 +54,13 @@ public class PreferencesDialog : Adw.Window
     private void Hide(Gtk.Widget sender, EventArgs e)
     {
         _controller.Theme = (Theme)_rowTheme.GetSelected();
+        _application.StyleManager!.ColorScheme = _controller.Theme switch
+        {
+            Theme.System => Adw.ColorScheme.PreferLight,
+            Theme.Light => Adw.ColorScheme.ForceLight,
+            Theme.Dark => Adw.ColorScheme.ForceDark,
+            _ => Adw.ColorScheme.PreferLight
+        };
         _controller.SaveConfiguration();
         Destroy();
     }
