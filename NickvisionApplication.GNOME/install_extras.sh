df1a9bfdbc99356881938879e3653a8c2846de7b
Fyodor Sobolev <117388856+fsobolev@users.noreply.github.com>
2023-01-05 22:21:14 +0300

    GNOME - Update install_extras.sh

--- NickvisionApplication.GNOME/install_extras.sh
+++ NickvisionApplication.GNOME/install_extras.sh
@@ -1,37 +1,53 @@
-#!/bin/bash
+#!/bin/sh
 
 INSTALL_PREFIX="/usr"
-if [ ! -z $1 ]
+if [ -n "${1}" ]
 then
-	INSTALL_PREFIX=$1
+    INSTALL_PREFIX="${1}"
 fi
-echo Install prefix: $INSTALL_PREFIX
+echo Install prefix: "${INSTALL_PREFIX}"
 
-if [ ${PWD##*/} == "NickvisionApplication.GNOME" ]
+if [ "$(basename "$(pwd)")" = "NickvisionApplication.GNOME" ]
 then
-	cd ..
+    cd ..
 fi
 
-echo Installing icons...
-mkdir -p $INSTALL_PREFIX/share/icons/hicolor/scalable/apps
-cp ./NickvisionApplication.Shared/Resources/org.nickvision.application.svg $INSTALL_PREFIX/share/icons/hicolor/scalable/apps/
-cp ./NickvisionApplication.Shared/Resources/org.nickvision.application-devel.svg $INSTALL_PREFIX/share/icons/hicolor/scalable/apps/
-mkdir -p $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps
-cp ./NickvisionApplication.Shared/Resources/org.nickvision.application-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
-cp ./NickvisionApplication.Shared/Resources/moon-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
-cp ./NickvisionApplication.Shared/Resources/sun-alt-symbolic.svg $INSTALL_PREFIX/share/icons/hicolor/symbolic/apps/
+echo "Installing icons..."
+mkdir -p "${INSTALL_PREFIX}"/share/icons/hicolor/scalable/apps
+for icon in org.nickvision.application.svg org.nickvision.application-devel.svg
+do
+    cp ./NickvisionApplication.Shared/Resources/${icon}               \
+       "${INSTALL_PREFIX}"/share/icons/hicolor/scalable/apps/
+done
+mkdir -p "${INSTALL_PREFIX}"/share/icons/hicolor/symbolic/apps
+for icon in org.nickvision.application-symbolic.svg  \
+                moon-symbolic.svg                    \
+                sun-alt-symbolic.svg
+do
+    cp ./NickvisionApplication.Shared/Resources/${icon}               \
+       "${INSTALL_PREFIX}"/share/icons/hicolor/symbolic/apps/
+done
 
-#echo Installing GResource...
-#mkdir -p $INSTALL_PREFIX/share/org.nickvision.application
-#glib-compile-resources ./NickvisionApplication.GNOME/Resources/org.nickvision.application.gresource.xml
-#mv ./NickvisionApplication.GNOME/Resources/org.nickvision.application.gresource $INSTALL_PREFIX/share/org.nickvision.application/
+# echo "Installing GResource..."
+# mkdir -p "${INSTALL_PREFIX}"/share/org.nickvision.application
+# glib-compile-resources ./NickvisionApplication.GNOME/Resources/org.nickvision.application.gresource.xml
+# mv ./NickvisionApplication.GNOME/Resources/org.nickvision.application.gresource \
+#    "${INSTALL_PREFIX}"/share/org.nickvision.application/
 
-echo Installing desktop file...
-mkdir -p $INSTALL_PREFIX/share/applications
-cp ./NickvisionApplication.GNOME/org.nickvision.application.desktop $INSTALL_PREFIX/share/applications/
+echo "Installing desktop file..."
+mkdir -p "${INSTALL_PREFIX}"/share/applications
+cp ./NickvisionApplication.GNOME/org.nickvision.application.desktop \
+   "${INSTALL_PREFIX}"/share/applications/
 
-echo Installing metainfo...
-mkdir -p $INSTALL_PREFIX/share/metainfo
-cp ./NickvisionApplication.GNOME/org.nickvision.application.metainfo.xml $INSTALL_PREFIX/share/metainfo/
+echo "Installing metainfo..."
+mkdir -p "${INSTALL_PREFIX}"/share/metainfo
+cp ./NickvisionApplication.GNOME/org.nickvision.application.metainfo.xml    \
+   "${INSTALL_PREFIX}"/share/metainfo/
 
-echo Done!
+# echo "Installing mime types..."
+# mkdir -p "${INSTALL_PREFIX}"/share/mime/packages
+# cp ./NickvisionApplication.GNOME/org.nickvision.application.extension.xml   \
+#    "${INSTALL_PREFIX}"/share/mime/packages/
+# update-mime-database "${INSTALL_PREFIX}"/share/mime/
+
+echo "Done!"
