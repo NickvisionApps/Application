b825af7fa3e19b49b1088102c3982886f114c962
Nick Logozzo <nlogozzo225@gmail.com>
2023-05-20 17:27:01 -0400

    Fluent - Handle Open & Closing Folder

--- NickvisionApplication.Fluent/Views/MainWindow.axaml.cs
+++ NickvisionApplication.Fluent/Views/MainWindow.axaml.cs
@@ -1,7 +1,11 @@
 using Avalonia.Controls;
 using Avalonia.Interactivity;
-using FluentIcons.Common;
+using FluentAvalonia.UI.Controls;
 using NickvisionApplication.Shared.Controllers;
+using NickvisionApplication.Shared.Events;
+using Symbol = FluentIcons.Common.Symbol;
+using System;
+using System.IO;
 
 namespace NickvisionApplication.Fluent.Views;
 
@@ -11,6 +15,7 @@ namespace NickvisionApplication.Fluent.Views;
 public partial class MainWindow : Window
 {
     private readonly MainWindowController _controller;
+    private EventHandler<RoutedEventArgs>? _notificationButtonClickEvent;
 
     /// <summary>
     /// Constructs a MainWindow
@@ -31,6 +36,9 @@ public partial class MainWindow : Window
         InitializeComponent();
         _controller = controller;
         Title = _controller.AppInfo.ShortName;
+        //Register Events
+        _controller.NotificationSent += NotificationSent;
+        _controller.FolderChanged += FolderChanged;
         //Localize Strings
         MenuFile.Header = _controller.Localizer["File"];
         MenuOpenFolder.Header = _controller.Localizer["OpenFolder"];
@@ -45,6 +53,83 @@ public partial class MainWindow : Window
         StatusPageHome.Title = _controller.Greeting;
         StatusPageHome.Description = _controller.Localizer["NoFolderDescription"];
     }
+
+    /// <summary>
+    /// Occurs when a notification is sent from the controller
+    /// </summary>
+    /// <param name="sender">object?</param>
+    /// <param name="e">NotificationSentEventArgs</param>
+    private void NotificationSent(object? sender, NotificationSentEventArgs e)
+    {
+        InfoBar.Message = e.Message;
+        InfoBar.Severity = e.Severity switch
+        {
+            NotificationSeverity.Informational => InfoBarSeverity.Informational,
+            NotificationSeverity.Success => InfoBarSeverity.Success,
+            NotificationSeverity.Warning => InfoBarSeverity.Warning,
+            NotificationSeverity.Error => InfoBarSeverity.Error,
+            _ => InfoBarSeverity.Informational
+        };
+        if (_notificationButtonClickEvent != null)
+        {
+            BtnInfoBar.Click -= _notificationButtonClickEvent;
+        }
+        BtnInfoBar.IsVisible = !string.IsNullOrEmpty(e.Action);
+        if (e.Action == "close")
+        {
+            BtnInfoBar.Content = _controller.Localizer["Close"];
+            _notificationButtonClickEvent = (_, _) => _controller.CloseFolder();
+            BtnInfoBar.Click += _notificationButtonClickEvent;
+        }
+        InfoBar.IsOpen = true;
+    }
+
+    /// <summary>
+    /// Occurs when the folder in the application changes
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void FolderChanged(object? sender, EventArgs e)
+    {
+        if(Directory.Exists(_controller.FolderPath))
+        {
+            Carousel.Next();
+            MenuCloseFolder.IsEnabled = true;
+            IconStatus.Symbol = Symbol.Folder;
+        }
+        else
+        {
+            Carousel.Previous();
+            MenuCloseFolder.IsEnabled = false;
+            IconStatus.Symbol = Symbol.Checkmark;
+        }
+        LblStatus.Text = _controller.FolderPath;
+    }
+
+    /// <summary>
+    /// Occurs when the open folder menu item is clicked
+    /// </summary>
+    /// <param name="sender">object?</param>
+    /// <param name="e">RoutedEventArgs</param>
+    private async void OpenFolder(object? sender, RoutedEventArgs e)
+    {
+        var openFolderDialog = new OpenFolderDialog()
+        {
+            Title = _controller.Localizer["OpenFolder"]
+        };
+        var result = await openFolderDialog.ShowAsync(this);
+        if (result != null)
+        {
+            _controller.OpenFolder(result);
+        }
+    }
+
+    /// <summary>
+    /// Occurs when the close folder menu item is clicked
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void CloseFolder(object? sender, RoutedEventArgs e) => _controller.CloseFolder();
 
     /// <summary>
     /// Occurs when the exit menu item is clicked
