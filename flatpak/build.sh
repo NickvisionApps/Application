0648f629b574bd2b7873ede219e359229c66ae64
Nick Logozzo <nlogozzo225@gmail.com>
2023-05-22 14:37:07 -0400

    Update build.sh

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -6,12 +6,12 @@ then
     exit 1
 fi
 
-if [ -d "$FLATPAK_BUILDER_BUILDDIR/cake-sources" ]
+if [ -d "cake-sources" ]
 then
     mkdir -p tools/Addins/Cake.FileHelpers.6.1.3
     unzip -o cake-sources/cake.filehelpers.6.1.3.nupkg -d tools/Addins/Cake.FileHelpers.6.1.3/
-    dotnet tool restore --add-source $FLATPAK_BUILDER_BUILDDIR/cake-sources
-    dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained --sources=$FLATPAK_BUILDER_BUILDDIR/nuget-sources
+    dotnet tool restore --add-source cake-sources
+    dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained --sources=nuget-sources
 else
     dotnet tool restore
     dotnet cake --target=Publish --prefix=/app --ui=$1 --self-contained
