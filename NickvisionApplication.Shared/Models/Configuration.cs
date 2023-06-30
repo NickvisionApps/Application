e454ed1545bb1e45fefcefa39f2020704ccdb7ec
Nick Logozzo <nlogozzo225@gmail.com>
2023-06-29 21:25:58 -0400

    Shared - Better Config Path

--- NickvisionApplication.Shared/Models/Configuration.cs
+++ NickvisionApplication.Shared/Models/Configuration.cs
@@ -9,7 +9,7 @@ namespace NickvisionApplication.Shared.Models;
 /// </summary>
 public class Configuration
 {
-    public static readonly string ConfigDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}Nickvision{Path.DirectorySeparatorChar}{AppInfo.Current.Name}";
+    public static readonly string ConfigDir = $"{Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)}{Path.DirectorySeparatorChar}{AppInfo.Current.Name}";
     private static readonly string ConfigPath = $"{ConfigDir}{Path.DirectorySeparatorChar}config.json";
     private static Configuration? _instance;
 
