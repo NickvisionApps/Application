68a86fd54157ee83239fa3d208483cce97c2b63a
Nick Logozzo <nlogozzo225@gmail.com>
2023-07-01 09:05:46 -0400

    Hello July!

--- NickvisionApplication.GNOME/Program.cs
+++ NickvisionApplication.GNOME/Program.cs
@@ -48,7 +48,7 @@ public partial class Program
         _mainWindowController.AppInfo.Name = "NickvisionApplication";
         _mainWindowController.AppInfo.ShortName = _("Application");
         _mainWindowController.AppInfo.Description = $"{_("Create new Nickvision applications")}.";
-        _mainWindowController.AppInfo.Version = "2023.6.0-next";
+        _mainWindowController.AppInfo.Version = "2023.7.0-next";
         _mainWindowController.AppInfo.Changelog = "<ul><li>Initial Release</li></ul>";
         _mainWindowController.AppInfo.GitHubRepo = new Uri("https://github.com/NickvisionApps/Application");
         _mainWindowController.AppInfo.IssueTracker = new Uri("https://github.com/NickvisionApps/Application/issues/new");
