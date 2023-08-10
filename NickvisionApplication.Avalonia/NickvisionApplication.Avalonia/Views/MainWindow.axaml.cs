28c87321e41a4a2b9322aa78cbcf2756935e0a5c
Nick Logozzo <nlogozzo225@gmail.com>
2023-08-10 16:57:12 -0400

    Avalonia - Add Home Page

--- NickvisionApplication.Avalonia/NickvisionApplication.Avalonia/Views/MainWindow.axaml.cs
+++ NickvisionApplication.Avalonia/NickvisionApplication.Avalonia/Views/MainWindow.axaml.cs
@@ -1,17 +1,28 @@
 using Avalonia.Controls;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Interactivity;
+using FluentAvalonia.UI.Controls;
 using NickvisionApplication.Shared.Controllers;
+using NickvisionApplication.Shared.Events;
 using System;
+using static NickvisionApplication.Shared.Helpers.Gettext;
 
 namespace NickvisionApplication.Avalonia.Views;
 
+/// <summary>
+/// The MainWindow for the application
+/// </summary>
 public partial class MainWindow : Window
 {
     private readonly MainWindowController _controller;
     private readonly IApplicationLifetime _lifetime;
     private bool _opened;
     
+    /// <summary>
+    /// Constructs a MainWindow
+    /// </summary>
+    /// <param name="controller">MainWindowController</param>
+    /// <param name="lifetime">IApplicationLifetime</param>
     public MainWindow(MainWindowController controller, IApplicationLifetime lifetime)
     {
         InitializeComponent();
@@ -20,10 +31,21 @@ public partial class MainWindow : Window
         _opened = false;
         //Window Settings
         Title = $"{_controller.AppInfo.ShortName}{(_controller.AppInfo.IsDevVersion ? " - DEV" : "")}";
-        Width = 800;
-        Height = 600;
+        //Register Events
+        _controller.NotificationSent += NotificationSent;
+        //Header
+        BtnOpenFolder.Label = _("Open");
+        ToolTip.SetTip(BtnSettings, _("Settings"));
+        //Greeting
+        GreetingTitle.Text = _controller.Greeting;
+        GreetingDescription.Text = _("Open a folder (or drag one into the app) to get started");
     }
 
+    /// <summary>
+    /// Occurs when the main content is loaded
+    /// </summary>
+    /// <param name="sender">object?</param>
+    /// <param name="e">RoutedEventArgs</param>
     private void MainContent_OnLoaded(object? sender, RoutedEventArgs e)
     {
         if (!_opened)
@@ -31,5 +53,41 @@ public partial class MainWindow : Window
             _controller.Startup(_lifetime is IClassicDesktopStyleApplicationLifetime desktop ? (desktop.Args ?? Array.Empty<string>()) : Array.Empty<string>());
             _opened = true;
         }
+    }
+
+    /// <summary>
+    /// Occurs when a notification is sent from the controller
+    /// </summary>
+    /// <param name="sender">object?</param>
+    /// <param name="e">NotificationSentEventArgs</param>
+    private void NotificationSent(object? sender, NotificationSentEventArgs e)
+    {
+        InfoBar.Title = e.Message;
+        InfoBar.Severity = e.Severity switch
+        {
+            NotificationSeverity.Success => InfoBarSeverity.Success,
+            NotificationSeverity.Error => InfoBarSeverity.Error,
+            NotificationSeverity.Warning => InfoBarSeverity.Warning,
+            _ => InfoBarSeverity.Informational
+        };
+        if (e.Action == "close")
+        {
+            var button = new Button()
+            {
+                Content = _("Close")
+            };
+            button.Click += (s, ex) => CloseFolder();
+            InfoBar.ActionButton = button;
+        }
+        else
+        {
+            InfoBar.ActionButton = null;
+        }
+        InfoBar.IsOpen = true;
+    }
+
+    private void CloseFolder()
+    {
+        
     }
 }
