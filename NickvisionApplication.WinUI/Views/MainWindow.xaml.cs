dde250ce23220d29cfb53209a9a54647c8b84ce8
Nick Logozzo <nlogozzo225@gmail.com>
2023-09-21 23:36:17 -0400

    WinUI - Remove MSIX Packaging Stuff

---
+++ NickvisionApplication.WinUI/Views/MainWindow.xaml.cs
@@ -0,0 +1,14 @@
+using Microsoft.UI.Xaml;
+
+namespace NickvisionApplication.WinUI.Views;
+
+/// <summary>
+/// An empty window that can be used on its own or navigated to within a Frame.
+/// </summary>
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
