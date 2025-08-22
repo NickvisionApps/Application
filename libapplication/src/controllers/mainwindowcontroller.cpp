6a43faf415d88c53e92ba5ea3add6457be707f13
Nick Logozzo <nlogozzo225@gmail.com>
2025-08-21 20:33:06 -0400

    feat: Update deps

--- libapplication/src/controllers/mainwindowcontroller.cpp
+++ libapplication/src/controllers/mainwindowcontroller.cpp
@@ -177,8 +177,11 @@ namespace Nickvision::Application::Shared::Controllers
                 {
                     return true;
                 }
-                m_appUpdateProgressChanged.invoke({ static_cast<double>(static_cast<long double>(downloadNow) / static_cast<long double>(downloadTotal)) });
-                return true;
+                double progress{ static_cast<double>(static_cast<long double>(downloadNow) / static_cast<long double>(downloadTotal)) };
+                if(progress != 1.0)
+                {
+                    m_appUpdateProgressChanged.invoke({ progress });
+                }
             } }) };
             m_appUpdateProgressChanged.invoke({ 1.0 });
             if(!res)
