715c04c7f964a5cc5f830964901e31441e455314
Nick Logozzo <nlogozzo225@gmail.com>
2026-08-03 19:12:48 +0200

    chore: Cleanup

--- linux/src/lib.rs
+++ linux/src/lib.rs
@@ -2,7 +2,7 @@ mod views;
 
 use crate::views::MainWindow;
 use adw::{Application, ColorScheme, prelude::*};
-use glib::ExitCode;
+use glib::{ExitCode, clone};
 use shared::{APP_ID, AppState, ApplicationTheme};
 use std::{cell::RefCell, rc::Rc};
 
@@ -10,19 +10,20 @@ pub fn run() -> ExitCode {
     let state = Rc::new(RefCell::new(AppState::default()));
     gio::resources_register_include!("compiled.gresource").unwrap();
     let app: Application = Application::builder().application_id(APP_ID).build();
-    let startup_state = state.clone();
-    let activate_state = state.clone();
-    app.connect_startup(move |app| {
-        let state_ref = startup_state.borrow();
-        app.style_manager()
-            .set_color_scheme(match state_ref.configuration().theme() {
-                ApplicationTheme::Light => ColorScheme::ForceLight,
-                ApplicationTheme::Dark => ColorScheme::ForceDark,
-                _ => ColorScheme::Default,
-            });
-    });
+    app.connect_startup(clone!(
+        #[strong] state,
+        move |app| {
+            let state_ref = state.borrow();
+            app.style_manager()
+                .set_color_scheme(match state_ref.configuration().theme() {
+                    ApplicationTheme::Light => ColorScheme::ForceLight,
+                    ApplicationTheme::Dark => ColorScheme::ForceDark,
+                    _ => ColorScheme::Default,
+                });
+        }
+    ));
     app.connect_activate(move |app| {
-        let window = MainWindow::new(app, activate_state.clone());
+        let window = MainWindow::new(app, state.clone());
         window.present();
     });
     app.run()
